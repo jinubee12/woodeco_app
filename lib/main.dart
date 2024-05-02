feb0c9c3efcc381bd47c0b8e235d9ba880b1af1e
jinubee12 <jinubee12@naver.com>
2024-05-03 00:20:14 +0900

    main_page

--- lib/main.dart
+++ lib/main.dart
@@ -16,7 +16,7 @@ class MyApp extends StatelessWidget {
     return MaterialApp(
       title: 'woodeco',
       debugShowCheckedModeBanner: false,
-      initialRoute: '/signin',
+      initialRoute: '/main',
       routes: {
         '/main': (context) => const MainPage(),
         '/signin': (context) => const SignInPage(),
